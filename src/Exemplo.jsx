922bd938b00c8e5db2e0d98d9142bdb66ab1a4f5
Marcela Torro <99346289+MaahTorro@users.noreply.github.com>
2023-04-17 12:06:22 -0300

    mudar cor

--- src/Exemplo.jsx
+++ src/Exemplo.jsx
@@ -3,12 +3,13 @@ import { useState } from "react"
 const Exemplo = ({cor}) =>{
 
     const [texto, setTexto] = useState("Texto do exemplo")
-    
+    const [corNova, setCorNova] = useState("black")
 
     return (
         <div>
-            <h1 style={{color: cor}}>{texto}</h1>
+            <h1 style={{color: corNova}}>{texto}</h1>
             <button onClick={()=>{setTexto("Mudando via botão")}}>Mudar</button>
+            <button onClick={() => {setCorNova("red")}}>Mudar cor</button>
             </div>
     )
 }
